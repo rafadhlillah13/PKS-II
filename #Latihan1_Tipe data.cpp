3752a18965d9f4ee1a01a84ffcb6f4b9c367620a
RAFI FADHLILLAH <rafifadhlillah5@gmail.com>
2022-03-05 14:06:28 +0700

    Add files via upload

---
+++ #Latihan1_Tipe data.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout<< "	Tipe-tipe data	"<<endl;
+	cout<<endl;
+	//integer (bilangan bulat)
+	int a;
+	//float (bilangan desimal, bisa juga bilangan bulat)
+	float b;
+	//char (karakter berisi satu huruf
+	char c;
+	//string ( kalimat )
+	string d;
+	// boolean (true or false)
+	bool e;
+	
+	cout<< "masukkan a = ";
+	cin>>a;
+	cout<< "masukkan b = ";
+	cin>>b;
+	cout<< "masukkan c = ";
+	cin>>c;
+	cout<< "masukkan d = ";
+	cin>>d;
+	cout<< "masukkan e = ";
+	cin>>e;
+	
+	cout<< "a = "<< a << endl;
+	cout<< "b = "<< b << endl;
+	cout<< "c = "<< c << endl;
+	cout<< "d = "<< d << endl;
+	cout<< "e = "<< e << endl;
+	
+	
+	
+	return 0;
+}
