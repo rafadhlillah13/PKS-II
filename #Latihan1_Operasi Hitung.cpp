09469a91af8b17835ccf6ae99bf9ad6c38009772
RAFI FADHLILLAH <rafifadhlillah5@gmail.com>
2022-03-05 14:16:08 +0700

    Add files via upload

---
+++ #Latihan1_Operasi Hitung.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cout << "Inputkan nilai a: ";
+    cin >> a;
+
+    cout << "Inputkan nilai b: ";
+    cin >> b;
+
+    cout << "Hasil a + b: " << a + b << endl;
+    cout << "Hasil a - b: " << a - b << endl;
+    cout << "Hasil a * b: " << a * b << endl;
+    cout << "Hasil a / b: " << a / b << endl;
+    cout << "Hasil a % b: " << a % b << endl;
+
+    return 0;
+}
